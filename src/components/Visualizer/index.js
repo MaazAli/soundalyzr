7bd36c543caa4e9390ed4373ca141884e8d515e3
MaazAli <maaz_ali@outlook.com>
2016-08-01 23:47:23 -0700

    Add ability to switch between visualizations

--- src/components/Visualizer/index.js
+++ src/components/Visualizer/index.js
@@ -7,18 +7,56 @@ import ElectrifiedLine from '../ElectrifiedLine'
 class Visualizer extends Component {
   constructor(props) {
     super(props)
-    // this.vizMap = {
-    //   "default": <EnergizedParticles analyser={this.props.analyser} />,
-    //   "EnergizedParticles": <EnergizedParticles analyser={this.props.analyser} />
-    // }
+    this.state = {
+      currentViz: this.getDefaultViz()
+    }
+    this.createVizMap.bind(this)
+
+    // Initialize visualization map
+    this.createVizMap(this.props)
+  }
+
+  createVizMap(props) {
+    this.vizMap = {
+      "EnergizedParticles": <EnergizedParticles analyser={props.analyser} />,
+      "ElectrifiedLine": <ElectrifiedLine analyser={props.analyser} />
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    // TODO: HACKY, will need to figure out how to inject the updated analyser
+    // to all components in the map when it changes, instead of re-creating the whole thing
+    // every time
+    this.createVizMap(nextProps)
+  }
+
+  getDefaultViz() {
+    return "EnergizedParticles"
+  }
+
+  onChangeViz(newViz) {
+    console.log(newViz)
+    this.setState({currentViz: newViz})
   }
 
   render() {
-    const {visualizationName, analyser} = this.props
-    // const $visualization = this.vizMap[visualizationName || "default"]
+    const { currentViz } = this.state
+    const $visualization = this.vizMap[currentViz]
+
+    const curStyle = {
+      borderBottom: "1px solid"
+    }
     return (
       <div>
-        <EnergizedParticles analyser={this.props.analyser} />
+        <div className={styles.vizSelector}>
+          <span style={currentViz === "EnergizedParticles" ? curStyle : {}} onClick={() => this.onChangeViz("EnergizedParticles")}>
+            Energized Particles
+          </span>
+          <span style={currentViz === "ElectrifiedLine" ? curStyle : {}} onClick={() => this.onChangeViz("ElectrifiedLine")}>
+            Electrified Line
+          </span>
+        </div>
+        {$visualization}
       </div>
     )
   }
