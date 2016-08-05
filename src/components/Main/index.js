e423733bb56e138d3ca04cd8e3c96d1cbbfee3cd
MaazAli <maaz_ali@outlook.com>
2016-08-04 22:30:50 -0700

    Add url query param, to be able to link to specific song visualizations

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,6 +1,7 @@
-import styles from './style.scss'
-
 import React, { Component } from 'react'
+import QueryString from 'query-string'
+
+import styles from './style.scss'
 
 import Visualizer from '../Visualizer'
 import SongSelector from '../SongSelector'
@@ -9,18 +10,27 @@ import AudioPlayer from '../AudioPlayer'
 export default class Main extends Component {
   constructor(props) {
     super(props)
+
+    const qs = QueryString.parse(window.location.search)
+
     this.state = {
-      defaultSongUrl: "https://soundcloud.com/blvksheepmusic/alesso-hereos-blvk-sheep-trap-remix",
+      defaultSongUrl: qs.url || "https://soundcloud.com/blvksheepmusic/alesso-hereos-blvk-sheep-trap-remix",
       songRecord: null,
       analyser: null
     }
     this.onHandleSongChange.bind(this)
   }
   onHandleSongChange(songRecord) {
+    this.updateQueryString(songRecord.permalink_url)
     this.setState({songRecord: songRecord})
   }
   onAnalyserChange(analyser) {
     this.setState({analyser: analyser})
+  }
+  updateQueryString(url) {
+    const { protocol, host, pathname } = window.location
+    let newUrl = `${protocol}//${host}${pathname}?${QueryString.stringify({url: url})}`
+    window.history.pushState({path:newUrl},'',newUrl)
   }
   render() {
     const { defaultSongUrl, analyser, songRecord } = this.state
